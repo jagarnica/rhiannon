c0f780de98f34dc5e4774bffaa2f2fcf4faace57
Jacob Killelea <jkillelea@protonmail.ch>
2020-04-09 23:21:21 -0700

    enumerate all audio input formats

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use cpal;
+use cpal::traits::*;
+
 fn main() {
-    println!("Hello, world!");
+    let host = cpal::default_host();
+
+    // enumerate all available input formats
+    let device = host.default_input_device()
+        .expect("no input device found");
+
+    if let Ok(mut fmt_range) = device.supported_input_formats() {
+        while let Some(fmt) = fmt_range.next() {
+            println!("{:?}", fmt);
+        }
+    }
 }
