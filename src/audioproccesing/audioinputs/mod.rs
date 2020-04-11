1490742911555068c539ded722a868fb0471b0ee
Jacob Killelea <jkillelea@protonmail.ch>
2020-04-10 22:16:09 -0700

    style tweaks (leading underscores only for unused vars)

--- src/audioproccesing/audioinputs/mod.rs
+++ src/audioproccesing/audioinputs/mod.rs
@@ -2,9 +2,8 @@ use cpal;
 use cpal::traits::*;
 
 pub fn list_inputs() {
-    let _host = cpal::default_host();
     // enumerate all available input formats
-    let device = _host.default_input_device()
+    let device = cpal::default_host().default_input_device()
         .expect("no input device found");
 
     if let Ok(mut fmt_range) = device.supported_input_formats() {
@@ -15,10 +14,9 @@ pub fn list_inputs() {
 }
 
 pub fn list_hosts(){
-    let _hosts_found = cpal::available_hosts();
     // Get the result of available hosts and then list them. 
-    let _string_of_hosts: Vec<_> = _hosts_found
+    let string_of_hosts: Vec<_> = cpal::available_hosts()
     .into_iter()  
     .collect();
-    println!("Hosts Found: {:?}", _string_of_hosts);
+    println!("Hosts Found: {:?}", string_of_hosts);
 }
