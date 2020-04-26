b7515761a57136822e29f05d31dcf97729509a58
Jesus Garnica <gtechy100@gmail.com>
2020-04-25 19:58:37 -0700

    Added finding a host, Input device, and output device

--- src/audioproccesing/inputs/mod.rs
+++ src/audioproccesing/inputs/mod.rs
@@ -1,6 +1,8 @@
+
 use cpal;
-use cpal::traits::*;
+use cpal::traits::{DeviceTrait, HostTrait, StreamIdTrait};
 use anyhow;
+use ringbuf::RingBuffer;
 /// Enumerate all available input formats for the default device 
 pub fn list_default_input_formats() {
     let device = cpal::default_host().default_input_device()
@@ -101,4 +103,109 @@ pub fn enumerate_device_info() -> Result<(), anyhow::Error>{
     }
 
     Ok(())
+}
+
+/// Prints out a list of possible devices found on the current default host 
+pub fn list_available_devices() -> Result<(), anyhow::Error>{
+    // First lets get the default host 
+    let host = cpal::default_host();
+    // get the list of devices from the host 
+    let devices = host.devices()?;
+    println!("  Devices: ");
+    for (device_index, device) in devices.enumerate() {
+        println!("  {}. \"{}\"", device_index + 1, device.name()?);
+    }
+    Ok(())
+}
+/// Streams input to the output device 
+pub fn listen() -> Result<(), anyhow::Error> {
+    let latency_ms: f32 = 150.0;
+    let host = cpal::default_host();
+
+    // Default devices.
+    let input_device = host
+        .default_input_device()
+        .expect("failed to get default input device");
+    let output_device = host
+        .default_output_device()
+        .expect("failed to get default output device");
+    println!("Using default input device: \"{}\"", input_device.name()?);
+    println!("Using default output device: \"{}\"", output_device.name()?);
+
+    // We'll try and use the same configuration between streams to keep it simple.
+    let config: cpal::StreamConfig = input_device.default_input_config()?.into();
+
+    // Create a delay in case the input and output devices aren't synced.
+    let latency_frames = (latency_ms / 1_000.0) * config.sample_rate.0 as f32;
+    let latency_samples = latency_frames as usize * config.channels as usize;
+
+    // The buffer to share samples
+    let ring = RingBuffer::new(latency_samples * 2);
+    let (mut producer, mut consumer) = ring.split();
+
+    // Fill the samples with 0.0 equal to the length of the delay.
+    for _ in 0..latency_samples {
+        // The ring buffer has twice as much space as necessary to add latency here,
+        // so this should never fail
+        producer.push(0.0).unwrap();
+    }
+
+    let input_data_fn = move |data: &[f32]| {
+        let mut output_fell_behind = false;
+        for &sample in data {
+            if producer.push(sample).is_err() {
+                output_fell_behind = true;
+            }
+        }
+        if output_fell_behind {
+            eprintln!("output stream fell behind: try increasing latency");
+        }
+    };
+
+    let output_data_fn = move |data: &mut [f32]| {
+        let mut input_fell_behind = None;
+        for sample in data {
+            *sample = match consumer.pop() {
+                Ok(s) => s,
+                Err(err) => {
+                    input_fell_behind = Some(err);
+                    0.0
+                }
+            };
+        }
+        if let Some(err) = input_fell_behind {
+            eprintln!(
+                "input stream fell behind: {:?}: try increasing latency",
+                err
+            );
+        }
+    };
+
+    // Build streams.
+    println!(
+        "Attempting to build both streams with f32 samples and `{:?}`.",
+        config
+    );
+    let input_stream = input_device.build_input_stream(&config, input_data_fn, err_fn)?;
+    let output_stream = output_device.build_output_stream(&config, output_data_fn, err_fn)?;
+    println!("Successfully built streams.");
+
+    // Play the streams.
+    println!(
+        "Starting the input and output streams with `{}` milliseconds of latency.",
+        latency_ms
+    );
+    input_stream.play()?;
+    output_stream.play()?;
+
+    // Run for 3 seconds before closing.
+    println!("Playing for 3 seconds... ");
+    std::thread::sleep(std::time::Duration::from_secs(3));
+    drop(input_stream);
+    drop(output_stream);
+    println!("Done!");
+    Ok(()
+}
+fn err_fn(err: cpal::StreamError) {
+    eprintln!("an error occurred on stream: {}", err);
 }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,13 @@ mod audioproccesing;
 
 fn main() {
     // enumerate all available input formats
-    audioproccesing::inputs::enumerate_device_info();
+    // audioproccesing::inputs::enumerate_device_info();
+    //audioproccesing::inputs::list_default_input_formats();
+    audioproccesing::inputs::list_available_devices();
+
+    audioproccesing::inputs::list_default_device();
+
+    audioproccesing::inputs::list_hosts();
+
    
 }
