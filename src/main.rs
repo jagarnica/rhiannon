506b19e236eddf2385168734299566dbd2218b29
Jacob Killelea <jkillelea@protonmail.ch>
2020-04-25 20:47:22 -0700

    testing getting a discord bot logged in

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,46 @@
 #![allow(unused)] // don't warn on unused things
+use std::env;
+
+use serenity::{
+    self,
+    prelude::*,
+    Client,
+    model::{
+        channel::Message,
+        gateway::Ready
+    },
+};
+
 use cpal;
 use cpal::traits::*;
-
 mod audioproccesing;
 
+struct Handler;
+
+impl EventHandler for Handler {
+    fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "pong!") {
+                println!("Send error: {}", why);
+            }
+        }
+    }
+
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected. \nData: {:#?}", ready.user.name, ready);
+    }
+}
+
 fn main() {
-    // enumerate all available input formats
-    audioproccesing::inputs::list_inputs();
-    audioproccesing::inputs::list_hosts();
+    let token = env::var("DISCORD_TOKEN").expect("No discord token found!");
+
+    let mut client = Client::new(&token, Handler).expect("Error creating client");
+
+    if let Err(why) = client.start() {
+        println!("Client error: {}", why);
+    }
+
+    // // enumerate all available input formats
+    // audioproccesing::inputs::list_inputs();
+    // audioproccesing::inputs::list_hosts();
 }
