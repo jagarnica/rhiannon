95fadc57ec830ae96b36f2481da40c8793d79625
Jesus Garnica <gtechy100@gmail.com>
2020-04-28 04:01:33 -0700

    Converted existing functions to rodio.

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,10 @@ fn main() {
     // audioproccesing::inputs::enumerate_device_info();
     //audioproccesing::inputs::list_default_input_formats();
     audioproccesing::inputs::list_available_devices();
-
-    audioproccesing::inputs::list_default_device();
+    audioproccesing::inputs::list_default_input_formats();
+    audioproccesing::inputs::list_default_output_device();
 
     audioproccesing::inputs::list_hosts();
-
+    
    
 }
