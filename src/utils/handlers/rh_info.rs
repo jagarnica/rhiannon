6b05db1fac70791054e479a34ca415e0cd40b0d2
Jesus Garnica <gtechy100@gmail.com>
2020-05-20 04:56:49 -0700

    Begin adding other commands. There is a bug with the math func.

--- src/utils/handlers/rh_info.rs
+++ src/utils/handlers/rh_info.rs
@@ -2,7 +2,7 @@ use crate::audioproccesing::inputs as ap;
 use crate::utils::cli::InfoOpts;
 ///* Processes the info command arguments. 
 pub fn handle_info(verbose: bool, opts: InfoOpts){
-    
+    let verbose_enabled = verbose;
     match (&opts) {
         inputs  =>{
           match opts.inputs {
@@ -19,9 +19,28 @@ pub fn handle_info(verbose: bool, opts: InfoOpts){
                 },
                 _ =>{},
             }
+        },
+        defaults =>{
+            match opts.defaults {
+                true =>{
+                    println!("Listing defaults");
+                    list_defaults(verbose_enabled);
+                },
+                _ =>{},
+            }
         }
         _ => (),
 
     }
    
+}
+// List the inputs founds for the device 
+fn list_inputs(verbose: bool){
+   //* TODO Finish listing inputs 
+    println!("Todo...")
+}
+
+fn list_defaults(verbose: bool){
+    ap::list_default_input_device();
+    ap::list_default_output_device();
 }
