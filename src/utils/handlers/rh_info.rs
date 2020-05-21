ce4a5eae1972eaad6e33011cad58913df20c3aac
Jacob Killelea <jkillelea@protonmail.ch>
2020-05-21 01:06:15 -0700

    more formatting

--- src/utils/handlers/rh_info.rs
+++ src/utils/handlers/rh_info.rs
@@ -1,42 +1,44 @@
 use crate::audioproccesing::inputs as ap;
+use crate::utils::cli;
 use crate::utils::cli::InfoOpts;
-use crate::utils::cli as cli;
-///* Processes the info command arguments. 
-pub fn handle_info(verbose: bool, opts: InfoOpts){
+
+/// Processes the info command arguments.
+pub fn handle_info(verbose: bool, opts: InfoOpts) {
     let verbose_enabled = verbose;
 
-    // User wants us to list defaults for the system 
+    // User wants us to list defaults for the system
     if (opts.defaults) {
         println!("listing defaults... ")
     }
-    // User wants us to list the output devices for the system 
-    if (opts.outputs){
+    // User wants us to list the output devices for the system
+    if (opts.outputs) {
         println!("listing outputs...")
     }
-    // List all the input devices for the system 
-    if (opts.inputs){
+    // List all the input devices for the system
+    if (opts.inputs) {
         println!("listing inputs...")
     }
-    // enumerate all the devices on the system 
-    if (opts.all){
+    // enumerate all the devices on the system
+    if (opts.all) {
         list_audio_details();
     }
-    
 }
-fn test_func(test_arg: bool){
+
+fn test_func(test_arg: bool) {
     println!("Testing!!!")
 }
-// List the inputs founds for the device 
-fn list_inputs(verbose: bool){
-   //* TODO Finish listing inputs 
+
+// List the inputs founds for the device
+fn list_inputs(verbose: bool) {
+    //* TODO Finish listing inputs
     println!("Todo...")
 }
 
-fn list_audio_details(){
+fn list_audio_details() {
     ap::enumerate_device_info();
 }
 
-fn list_defaults(verbose: bool){
+fn list_defaults(verbose: bool) {
     ap::list_default_input_device();
     ap::list_default_output_device();
 }
--- src/utils/handlers/rh_start.rs
+++ src/utils/handlers/rh_start.rs
@@ -1,9 +1,7 @@
 use crate::audioproccesing as ap;
 use crate::utils::cli::StartOpts;
 
-
-pub fn handle_start(verbose: bool, opts: StartOpts){
+pub fn handle_start(verbose: bool, opts: StartOpts) {
     let port_number = opts;
-  // eprint("Starting the bot..{:?},",port_number );
-
+    // eprint("Starting the bot..{:?},",port_number );
 }
